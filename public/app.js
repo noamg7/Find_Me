ab36a537fe8c452068b14dad3f3298fc93ee0c2a
Noam <noamg@cox.net>
2016-03-18 20:59:58 -0400

    find_me

--- public/app.js
+++ public/app.js
@@ -1,11 +1,28 @@
-angular.module('ngFind', []);
-.service('wikiService', function($http){
-  $http({
-    url: "https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json";
-    method: 'json' 
-  })
-}
+angular.module('ngFind', [])
+.controller('findMeCtrl', function($scope, $http){
+  $http.get("https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content")
+    .then(function(data){
+        $scope.query.title = "title";
+    })
+//.config(['$httpProvider', function($httpProvider) {
+  // $http({
+  //   url: "https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json",
+  //   method: 'json'
+  // });
+});
 
+// app.use(function(req, res, next){
+//   req.header('Access-Control-Allow-Origin', "*");
+//   req.header('Access-Control-Allow-Methods', "GET");
+//   req.header('Access-Control-Allow-Headers', "Content-Type");
+//   next();
+// })
+
+//
+// $scope.wikiTodo = function(){
+//
+// }
+// {{query.title}}
 
 
 //"https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json"
