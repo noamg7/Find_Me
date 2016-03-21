966001117d549f0e317cef3093d6d3686db5ee11
Noam <noamg@cox.net>
2016-03-21 14:14:00 -0400

    find_me

--- public/app.js
+++ public/app.js
@@ -7,27 +7,9 @@ angular.module('ngFind', [])
     })
 
 
-
-
-// .config(['$httpProvider', function($httpProvider) {
-//   $http({
-//     url: "https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json",
-//     method: 'json'
-//   });
 });
 
-// app.use(function(req, res, next){
-//   req.header('Access-Control-Allow-Origin', "*");
-//   req.header('Access-Control-Allow-Methods', "GET");
-//   req.header('Access-Control-Allow-Headers', "Content-Type");
-//   next();
-// })
 
-//
-// $scope.wikiTodo = function(){
-//
-// }
-// {{query.title}}
 
 
 //"https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json"
