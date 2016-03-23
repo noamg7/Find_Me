94144bd800f902b1b9b8eeef91f4e8cebfe65ed8
Noam <noamg@cox.net>
2016-03-22 20:27:40 -0400

    find_me

--- public/app.js
+++ public/app.js
@@ -1,24 +1,12 @@
 angular.module('ngFind', [])
 .controller('findMeCtrl', function($scope, $http){
   $scope.query = 'Albert Einstein';
-  $http.jsonp("https://en.wikipedia.org/w/api.php?action=query&titles="+$scope.query+"&prop=revisions&rvprop=content&format=json&callback=JSON_CALLBACK")
+  $http.jsonp("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=info%7Ccategories%7Cimages%7Ctemplates&titles=Albert+Einstein&callback=JSON_CALLBACK")
     .then(function(response){
-        console.log(response);
-        // $scope.query.categories.title = "title",
-        // $scope.query.images = "img",
-        // $scope.query.title = "Occupation",
-        // $scope.query.categories.title = "Birth",
-        // $scope.query.categories.title = "Death",
+        console.log(response.data);
+        $scope.data = response.data;
+
     })
-   var function(data){
-    $scope.query.categories.title = "title",
-    $scope.query.images.title = "img",
-    $scope.query.title = "Occupation",
-    $scope.query.categories.title = "Birth",
-    $scope.query.categories.title = "Death",
-   }
-
-
 
 });
 
