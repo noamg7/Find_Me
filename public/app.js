597ff0a47e6804ded19e4c7d484f057b24065332
Noam <noamg@cox.net>
2016-03-25 20:29:47 -0400

    find_me

--- public/app.js
+++ public/app.js
@@ -1,16 +1,25 @@
-angular.module('ngFind', [])
-.controller('findMeCtrl', function($scope, $http){
-  $scope.query = 'Albert Einstein';
-  $http.jsonp("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=info%7Ccategories%7Cimages%7Ctemplates&titles=Albert+Einstein&callback=JSON_CALLBACK")
-    .then(function(response){
-        console.log(response.data);
-        $scope.data = response.data;
+var app = angular.module('myApp', []);
 
-    })
+app.factory('wikiService', function($http) {
 
+    var wikiService = {
+        // get: function(country) {
+        //     return $http.jsonp('http://es.wikipedia.org/w/api.php?titles=' + country.name.toLowerCase() + '&rawcontinue=true&action=query&format=json&prop=extracts&callback=JSON_CALLBACK');
+        get: function(individual) {
+          return $http.jsonp('https://en.wikipedia.org/w/api.php?action=query&format=json&prop=info%7Ccategories%7Cimages%7Ctemplates&titles=' + individual.name+ '&callback=JSON_CALLBACK');
+        }
+    };
+
+    return wikiService;
 });
 
+app.controller('MainController', function($scope, wikiService) {
+  $scope.wikiTodo = function(){
+    wikiService.get($scope.search).then(function(data) {
+        console.log(data);
+        $scope.wikiData = data.data;
+    });
 
+  }
 
-
-//"https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json"
+});
